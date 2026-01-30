1d82f5c6430eb05c1044e00b63b9c9b6790be462
stiinternationallogistics-afk <sti.international.logistics@gmail.com>
2026-01-30 16:38:06 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,22 +1,17 @@
 import os
-import asyncio
 from telegram import Update
-from telegram.ext import (
-    Application,
-    CommandHandler,
-    ContextTypes,
-)
+from telegram.ext import Application, CommandHandler, ContextTypes
 
-TOKEN = os.environ.get("TG_TOKEN")
+TOKEN = os.getenv("TG_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("✅ Бот запущен и работает")
 
-async def main():
+def main():
     app = Application.builder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
-    await app.run_polling()
+    app.run_polling()
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
 
